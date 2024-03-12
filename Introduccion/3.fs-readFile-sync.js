3f92ab96ca98d85202be3139eeeba1fb0be9ee38
sebaslade <sebaslade29@gmail.com>
2024-03-12 16:32:29 -0500

    Agregando archivos

--- Introduccion/3.fs-readFile-sync.js
+++ Introduccion/3.fs-readFile-sync.js
@@ -2,7 +2,7 @@ const fs = require("node:fs")
 
 console.log("Leyendo el primer archivo...")
 
-const text = fs.readFileSync('./archivo.txt', 'utf-8')
+const text = fs.readFileSync('archivo.txt', 'utf-8')
 console.log("primer texto: ", text)
 
 console.log("-> Haciendo cosas mientras lee el archivo...");
@@ -10,3 +10,7 @@ console.log("-> Haciendo cosas mientras lee el archivo...");
 console.log("Leyendo el segundo archivo...")
 const secondText = fs.readFileSync("./archivo2.txt", "utf-8")
 console.log("primer texto: ", secondText)
+
+const textModified = text.replace(/mundo/ig, 'NodeJS')
+// Esto modificará el archivo original 'archivo.txt'
+fs.writeFileSync('archivo-modified.txt', textModified)
